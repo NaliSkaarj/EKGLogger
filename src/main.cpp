4f5a99da8cf074a73a2cf0a60da53f01ce5193ef
Bishop <bishop@tonietu.pl>
2025-10-07 16:19:28 +0200

    ECG PoC

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,18 @@
 #include <Arduino.h>
+#define LO_M D0         // Right arm
+#define LO_P D1         // Left arm
 
 void setup() {
   Serial.begin(115200);
-  Serial.println();
-  delay(10000);
-  Serial.printf("Flash chip size (real): %u bytes\n", ESP.getFlashChipRealSize());
-  Serial.printf("Flash chip size (IDE):  %u bytes\n", ESP.getFlashChipSize());
-  Serial.printf("Flash chip speed:       %u Hz\n", ESP.getFlashChipSpeed());
-  Serial.printf("CPU chip speed:         %u MHz\n", ESP.getCpuFreqMHz());
-  Serial.print( "Reset info:             ");
-  Serial.println(ESP.getResetInfo());
-  Serial.print( "Reset reason:           ");
-  Serial.println(ESP.getResetReason());
-  Serial.println(ESP.getFullVersion());
 }
 
 void loop() {
-  delay(20000);
-  Serial.println("*");
-  Serial.print( "Free stack: ");
-  Serial.println(ESP.getFreeContStack());
-  Serial.print( "Free heap:  ");
-  Serial.println(ESP.getFreeHeap());
-  // Serial.println(ESP.getMaxFreeBlockSize());
-  // Serial.println(ESP.getHeapFragmentation());
+  if( digitalRead(LO_M) == HIGH ) {
+    Serial.println( "Right Arm electrode error!" );
+  } else if( digitalRead(LO_P) == HIGH ) {
+    Serial.println( "Left Arm electrode error!" );
+  } else {
+    Serial.println( analogRead(A0) );
+  }
+  delay(10);
 }
