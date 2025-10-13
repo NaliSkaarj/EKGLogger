15744ac8349bdcb34cdd6126a82bfd2b00a90fc4
Bishop <bishop@tonietu.pl>
2025-10-13 21:22:29 +0200

    Plot script in QtGui

--- tools/EKG_plot.py
+++ tools/EKG_plot.py
@@ -1,8 +1,11 @@
 import serial
-import matplotlib.pyplot as plt
 from collections import deque
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+import sys
 import threading
 
+# --- konfiguracja UART ---
 PORT = "/dev/ttyUSB0"
 BAUDRATE = 115200
 MAX_POINTS = 500
@@ -11,13 +14,16 @@ data = deque([0]*MAX_POINTS, maxlen=MAX_POINTS)
 
 ser = serial.Serial(PORT, BAUDRATE, timeout=1)
 
-# --- konfiguracja wykresu ---
-fig, ax = plt.subplots()
-line, = ax.plot(data)
-ax.set_ylim(-10, 1100)
-ax.set_xlim(0, MAX_POINTS)
+# --- konfiguracja GUI ---
+app = QtGui.QApplication(sys.argv)
+win = pg.GraphicsLayoutWidget(show=True, title="Realtime EKG/ADC")
+win.resize(800, 400)
+plot = win.addPlot(title="ADC Values")
+curve = plot.plot(list(data))
+plot.setYRange(-10, 1100)
+plot.setXRange(0, MAX_POINTS)
 
-# --- wątek tylko do czytania UART ---
+# --- wątek do odczytu UART ---
 def serial_reader():
     while True:
         try:
@@ -31,15 +37,16 @@ def serial_reader():
 thread = threading.Thread(target=serial_reader, daemon=True)
 thread.start()
 
-# --- funkcja aktualizująca wykres ---
-def update(frame):
-    line.set_ydata(data)
-    line.set_xdata(range(len(data)))
-    return line,
+# --- funkcja aktualizacji wykresu ---
+def update():
+    curve.setData(list(data))  # szybka aktualizacja
+    # curve.setPos(0,0) # opcjonalnie przesuwanie okna
 
-# --- animacja w głównym wątku ---
-import matplotlib.animation as animation
-ani = animation.FuncAnimation(fig, update, interval=1, save_count=MAX_POINTS, cache_frame_data=False)
-plt.show()
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(1)  # odświeżanie co ~1 ms
 
-ser.close()
+# --- start aplikacji Qt ---
+if __name__ == '__main__':
+    QtGui.QApplication.instance().exec_()
+    ser.close()
